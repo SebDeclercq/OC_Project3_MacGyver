a9659dff7682b524217b20f8d39c2892fefbade2
declercq sebastien <sdq@afnor.fr>
2018-08-29 16:12:38 +0200

    Prompt : adding collected tools display + renaming class variables

--- ui/Prompt.py
+++ ui/Prompt.py
@@ -16,9 +16,9 @@ class Prompt(TextOnly):
     """Class managing a prompt UI. Inherits from TextOnly instead of UI
     in order to use its interact method (same behaviour expected). Prompt
     only renders a neat screen display."""
-    BLUE_BACKGROUND = '46'
-    GRAY_BACKGROUND = '43;30'
-    GREEN_BACKGROUND = '42'
+    TOOL_BACKGROUND = '46'
+    PAWN_BACKGROUND = '43;30'
+    EXIT_BACKGROUND = '42'
 
     @classmethod
     def display(cls, json: str) -> None:
@@ -30,9 +30,9 @@ class Prompt(TextOnly):
 
         legend = {
             'WALL': ['', Config.WALL_CHAR],
-            'EXIT': [cls.GREEN_BACKGROUND, Config.EXIT_CHAR],
-            'MACGYVER': [cls.GRAY_BACKGROUND, Config.PAWN_CHAR],
-            'TOOL': [cls.BLUE_BACKGROUND, Config.TOOL_CHAR],
+            'EXIT': [cls.EXIT_BACKGROUND, Config.EXIT_CHAR],
+            'MACGYVER': [cls.PAWN_BACKGROUND, Config.PAWN_CHAR],
+            'TOOL': [cls.TOOL_BACKGROUND, Config.TOOL_CHAR],
         }
 
         print('=' * 14)
@@ -42,17 +42,17 @@ class Prompt(TextOnly):
         print('=' * 14, '\n\n')
 
         data.matrix = cls._colorize_matrix(
-            data.matrix, data.new_position, cls.GRAY_BACKGROUND
+            data.matrix, data.new_position, cls.PAWN_BACKGROUND
         )
 
         for position in data.tools.values():
             if position is not None:
                 data.matrix = cls._colorize_matrix(
-                    data.matrix, position, cls.BLUE_BACKGROUND
+                    data.matrix, position, cls.TOOL_BACKGROUND
                 )
 
         data.matrix = cls._colorize_matrix(
-            data.matrix, data.exit_cell, cls.GREEN_BACKGROUND
+            data.matrix, data.exit_cell, cls.EXIT_BACKGROUND
         )
 
         if cls.movement_count <= 1:
@@ -60,17 +60,23 @@ class Prompt(TextOnly):
         else:
             print('%04d hits' % (cls.movement_count,), '\n\n')
 
+        tools = ['\033[%sm%s\033[0m ' % (cls.TOOL_BACKGROUND, type)
+                for type, position in data.tools.items() if position is None]
+        print('Tools : ', ''.join(tools), '\n\n' )
+
         for row in reversed(data.matrix):
             print('|' + '|'.join(row) + '|')
+        print('\n')
         for tool_type, position in data.tools.items():
             if position is None and tool_type not in cls.found_tools:
-                print('MacGyver has found a %s !' % tool_type)
+                print('\nMacGyver has found a %s !' % tool_type)
                 cls.found_tools.append(tool_type)
         if data.new_position == data.exit_cell:
             if data.freedom:
                 print(f"\033[42mMacGyver is FREEEE !\033[0m")
             else:
                 print(f"\033[41mMacGyver didn't get out...\033[0m")
+            print('\n')
         cls.movement_count += 1
 
     @classmethod
